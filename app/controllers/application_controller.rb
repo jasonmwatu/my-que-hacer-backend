2b92136208ca0a4db03972aa9e33bd8915d883f2
jasonmwatu <jasonmwatu@gmail.com>
2022-07-04 08:39:22 +0300

    re init git

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,107 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/' do
+    @res = "root page"
+  end
+
+  #
+  # Routes dealing with Todo table
+  #
+
+  get '/todos/:user_id' do
+    #get all todos of user_id = :user_id
+    #table: todos table
+    # @res = "method: get /id. route id is #{params[:user_id]}"
+
+    todos = Todo.where(:user_id => params[:user_id]).to_json
+  end
+
+  patch '/update/:todo_id' do
+
+    todo = Todo.find(params[:todo_id])
+  
+    params.each {|key,value|
+      
+      unless key == "todo_id"
+        todo.update("#{key}": "#{value}")
+      end
+
+    }
+
+    msg = {:success => "Edit on todo with id: #{params[:todo_id]} successful."}
+    @res = msg.to_json 
+  end
+
+  delete '/delete/:todo_id' do
+      #delete todo with todo_id = :todo_id
+      # @res = "method: delete delete/id. route id is #{params[:todo_id]}"
+    todo = Todo.find(params[:todo_id])
+    todo.destroy
+    todo.to_json
+  end
+
+  post '/create' do
+    #create a new todo with details sent by user
+    # @res = "method: post create/"
+
+    new_todo = Todo.create(
+      title: params[:title],
+      done: params[:done],
+      urgent: params[:urgent],
+      important: params[:important],
+      user_id: params[:user_id]
+    )
+
+    new_todo.json
+    
+    # msg = {:success => "New Todo with id: #{params[:todo_id]} created successfully."}
+    # @res = msg.to_json 
+  end
+
+  #
+  # Routes dealing with User table and authentication
+  #
+
+  post "/signin" do
+
+    if User.exists?(username: params[:username])
+
+      check_user = User.find_by_username(params[:username])
+      db_password = check_user[:password]
+
+      if db_password == params[:password]
+        # check if passwords match
+
+        msg = {:success => "Signed in #{params[:username] } successfully", :user_id => check_user[:id], :username => check_user[:username]} 
+        @res = msg.to_json
+      else
+        msg = {:signin_error => "Incorrect password!"}
+        @res = msg.to_json
+      end
+
+    else
+      msg = {:signin_error => "username: #{params[:username]} does not exists. Sign up instead"}
+      @res = msg.to_json
+    end
+
+  end
+
+  post "/signup" do
+    
+    unless User.exists?(username: params[:username])
+      new_user = User.create(
+          username: params[:username],
+          password: params[:password]
+        )
+      msg = {:success => "user #{params[:username]} created successfully"} 
+      @res = msg.to_json
+    else
+      msg = {:signup_error => "username: #{params[:username]} exists."}
+      @res = msg.to_json
+    end
+    
+  end
+
+end
+  
---
+++ db/seeds.rb
@@ -0,0 +1,77 @@
+# require 'bundler/inline'
+
+# gemfile do
+#     source 'https://rubygems.org'
+
+#     gem 'sinatra', require: 'sinatra/base'
+#     gem 'sqlite3'
+#     gem 'activerecord'
+#     gem 'sinatra-activerecord'
+# end
+
+# require File.expand_path('../../app/models/todo.rb',  __FILE__)
+# require File.expand_path('../../app/models/user.rb',  __FILE__)
+require File.expand_path('../../config/environment.rb',  __FILE__)
+
+puts "..starting seeding"
+
+User.destroy_all
+Todo.destroy_all
+
+user1 = User.create(
+    username:   "irene",
+    password:   "secret"
+)
+
+user2 = User.create(
+    username: "njeru",
+    password: "password"
+)
+
+id1 = User.find_by_username("irene")
+id2 = User.where(:username => "njeru").first
+
+puts "created user #{user1} with id: #{id1[:id]} and username: #{id1[:username]} and password: #{id1[:password]}"
+puts "created user #{user2} with id: #{id2[:id]}"
+
+def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z')
+    Array.new(number) { charset.sample }.join
+end
+
+10.times do
+    Todo.create(
+        title: generate_code(10),
+        done: false,
+        urgent: true,
+        important: false,
+        user_id: id1[:id]
+    )
+end
+10.times do
+    Todo.create(
+        title: generate_code(10),
+        done: false,
+        urgent: false,
+        important: true,
+        user_id: id2[:id]
+    )
+end
+
+todos = Todo.all
+
+user1_todos = User.find(id1[:id])
+todos1 = user1_todos.to_json(include: [:todos])
+
+todos2 = Todo.where(:user_id => id2[:id]).to_json
+# todos2 = todos21.to_json
+
+#puts "created todos.. #{todos}"
+puts "user1 todos.. #{todos1}"
+puts "user2 todos.. #{todos2}"
+
+puts "=================="
+
+puts "created user #{user1} with id: #{id1[:id]} and username: #{id1[:username]} and password: #{id1[:password]}"
+puts "created user #{user2} with id: #{id2[:id]}"
+
